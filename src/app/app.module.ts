74524dc449337ddd5a5d0577cd6ecb2c5334e0d6
MauricioGeinfo <mauricio@geinfo.com.br>
2019-12-09 14:30:34 -0300

    'Opa'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { PessoaService } from './pessoas/pessoa.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { ToastyModule } from 'ng2-toasty';
 
 import { AppComponent } from './app.component';
+import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { PessoasModule } from './pessoas/pessoas.module';
+import { LancamentoService } from './lancamentos/lancamento.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LancamentosModule,
+    PessoasModule,
+    CoreModule,
+    HttpClientModule,
+    ToastyModule.forRoot()
   ],
-  providers: [],
+  providers: [LancamentoService, PessoaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
